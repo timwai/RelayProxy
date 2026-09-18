c01f4edb5196e81327c62d2fb41057d8eb8a5d75
tingjusting <tingjusting@gmail.com>
2026-09-19 00:54:49 +0800

    test(perf): benchmark traffic accounting hot path

---
+++ internal/traffic/perf_benchmark_test.go
@@ -0,0 +1,34 @@
+package traffic
+
+import "testing"
+
+func BenchmarkRecordAddParallel32KiB(b *testing.B) {
+	registry := NewRegistry(8192, 512)
+	record := registry.Start(Metadata{Protocol: "tcp"})
+	record.Activate()
+	b.ReportAllocs()
+	b.SetBytes(32 * 1024)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			record.AddUpload(32 * 1024)
+		}
+	})
+	b.StopTimer()
+	_ = registry.Snapshot()
+}
+
+func BenchmarkRegistrySnapshot128Active(b *testing.B) {
+	registry := NewRegistry(8192, 512)
+	for i := 0; i < 128; i++ {
+		record := registry.Start(Metadata{Protocol: "tcp"})
+		record.Activate()
+		record.AddUpload(32 * 1024)
+		record.AddDownload(32 * 1024)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = registry.Snapshot()
+	}
+}

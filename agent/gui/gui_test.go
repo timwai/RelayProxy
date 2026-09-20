6f79aad7af6545b689ef4c61337f4a57b1aa6188
tingjusting <tingjusting@gmail.com>
2026-09-20 21:57:52 +0800

    test: lock simplified Agent window title

--- agent/gui/gui_test.go
+++ agent/gui/gui_test.go
@@ -12,3 +12,10 @@ func TestDesktopWindowDimensionsRemainResponsive(t *testing.T) {
 			MinimumWindowWidth, MinimumWindowHeight)
 	}
 }
+
+
+func TestDefaultWindowTitleIsProductNameOnly(t *testing.T) {
+	if DefaultWindowTitle != "RelayProxy" {
+		t.Fatalf("default window title = %q, want RelayProxy", DefaultWindowTitle)
+	}
+}

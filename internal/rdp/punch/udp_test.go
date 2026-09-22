e28e82019ad443279053d5cad4bdcf5bbbdc8acd
tingjusting <tingjusting@gmail.com>
2026-09-22 17:21:31 +0800

    style: gofmt direct path probe tests

--- internal/rdp/punch/udp_test.go
+++ internal/rdp/punch/udp_test.go
@@ -157,7 +157,6 @@ func TestPacketConnDesktopDomainKeepaliveIsIsolated(t *testing.T) {
 	}
 }
 
-
 func TestPacketConnMeasuresAuthenticatedKeepaliveRTT(t *testing.T) {
 	target, err := net.ListenUDP("udp4", &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 0})
 	if err != nil {

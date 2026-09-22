cdce289c397d37bf39e6da85dce8c52b5c3b40b4
tingjusting <tingjusting@gmail.com>
2026-09-22 11:16:21 +0800

    feat: model retained D3D11 decoder surfaces

--- agent/desktop/codec/decoder.go
+++ agent/desktop/codec/decoder.go
@@ -3,10 +3,30 @@ package codec
 import (
 	"context"
 	"errors"
+	"sync"
 	"time"
 )
 
 var ErrDecoderUnavailable = errors.New("video decoder unavailable")
+
+type D3D11Surface struct {
+	Resource    uintptr
+	Subresource uint32
+
+	releaseOnce sync.Once
+	release     func()
+}
+
+func (s *D3D11Surface) Close() {
+	if s == nil {
+		return
+	}
+	s.releaseOnce.Do(func() {
+		if s.release != nil {
+			s.release()
+		}
+	})
+}
 
 type DecodedFrame struct {
 	Format    PixelFormat
@@ -16,6 +36,17 @@ type DecodedFrame struct {
 	Stride    int
 	Timestamp time.Duration
 	Hardware  bool
+	D3D11     *D3D11Surface
+}
+
+func (f *DecodedFrame) Close() {
+	if f == nil {
+		return
+	}
+	if f.D3D11 != nil {
+		f.D3D11.Close()
+		f.D3D11 = nil
+	}
 }
 
 type Decoder interface {

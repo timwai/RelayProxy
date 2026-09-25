3ecbee4c01a85d2b3f0e160c8356d9cb218cf1b5
tingjusting <tingjusting@gmail.com>
2026-09-25 18:07:02 +0800

    feat: clone GPU candidate diagnostics in target snapshots

--- server/session/manager.go
+++ server/session/manager.go
@@ -65,6 +65,7 @@ func DesktopCapabilitiesForTarget(sess *DeviceSession, nativeRDP, relayDesktop b
 	caps.Captures = append([]protocol.DesktopCaptureCapability(nil), caps.Captures...)
 	caps.Codecs = protocol.CloneDesktopCodecCapabilities(caps.Codecs)
 	caps.GPU = protocol.CloneDesktopGPUCapability(caps.GPU)
+	caps.GPUCandidates = protocol.CloneDesktopGPUCandidateDiagnostics(caps.GPUCandidates)
 	caps.AudioCodecs = append([]string(nil), caps.AudioCodecs...)
 	caps.Displays = append([]protocol.DesktopDisplayCapability(nil), caps.Displays...)
 	return caps
